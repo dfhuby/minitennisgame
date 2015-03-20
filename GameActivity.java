39818b8aae768d74e7879c6e85198bd2d403b315
dfhuby <andrei.vargin@gmail.com>
2015-03-20 17:14:21 +0300

    Create GameActivity.java

---
+++ GameActivity.java
@@ -0,0 +1,14 @@
+package my.first.app.minitennisgame;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class GameActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
